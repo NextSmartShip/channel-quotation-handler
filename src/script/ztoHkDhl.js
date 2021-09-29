0a23cd03935b7040c1e65dc2971e1cec7fec426e
Brave <brave@fingersonezero.com>
2021-09-29 16:09:51 +0800

    update

--- src/script/ztoHkDhl.js
+++ src/script/ztoHkDhl.js
@@ -6,6 +6,11 @@ import countries from "../config/countries.json";
 export function getCountryZones() {
   return [
     {
+      org_zone: "dus",
+      zone: "",
+      countryCode: "US",
+    },
+    {
       org_zone: "zone1",
       zone: "ZTO_HK_DHL_1",
       countryCode: "MO",
@@ -1140,51 +1145,34 @@ export function handleZtoHkDhlExcelJson(json) {
     }
     for (const key in row) {
       if (Number(row[key]) > 0) {
-        if (key === "dus") {
-          const surcharge =
-            unitWeight === 0 ? Math.ceil(endWeight / 1000) * 8 : 0;
+        const countryCodes = countryZones.filter((cz) => cz.org_zone === key);
+        countryCodes.forEach((cc) => {
+          let surcharge = 0;
+          const findCountry = countries.data.find(
+            (item) => item.code === cc.countryCode
+          );
+
+          if (unitWeight === 0 && cc.countryCode !== "MO") {
+            if (["AU", "NZ"].includes(cc.countryCode)) {
+              surcharge = Math.ceil(endWeight / 1000) * 18;
+            } else {
+              surcharge = Math.ceil(endWeight / 1000) * 8;
+            }
+          }
+
           items.push({
             mode: unitWeight === 0 ? COST_MODE.TotalPrice : COST_MODE.UnitPrice,
-            country_code: "US",
-            country_name: "美国",
+            country_code: cc.countryCode,
+            country_name: findCountry ? findCountry.name_cn : "",
             start_weight: startWeight,
             end_weight: endWeight,
             first_weight: 0,
             first_weight_fee: 0,
             unit_weight: unitWeight,
             unit_weight_fee: row[key] + surcharge,
+            zone: cc.zone,
           });
-        } else if (key.indexOf("zone") > -1) {
-          const countryCodes = countryZones.filter((cz) => cz.org_zone === key);
-          countryCodes.forEach((cc) => {
-            let surcharge = 0;
-            const findCountry = countries.data.find(
-              (item) => item.code === cc.countryCode
-            );
-
-            if (unitWeight === 0 && cc.countryCode !== "MO") {
-              if (["AU", "NZ"].includes(cc.countryCode)) {
-                surcharge = Math.ceil(endWeight / 1000) * 18;
-              } else {
-                surcharge = Math.ceil(endWeight / 1000) * 8;
-              }
-            }
-
-            items.push({
-              mode:
-                unitWeight === 0 ? COST_MODE.TotalPrice : COST_MODE.UnitPrice,
-              country_code: cc.countryCode,
-              country_name: findCountry ? findCountry.name_cn : "",
-              start_weight: startWeight,
-              end_weight: endWeight,
-              first_weight: 0,
-              first_weight_fee: 0,
-              unit_weight: unitWeight,
-              unit_weight_fee: row[key] + surcharge,
-              zone: cc.zone,
-            });
-          });
-        }
+        });
       }
     }
     preWeight = endWeight;
