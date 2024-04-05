abbe88e7d2eaff4f574b962536584542e04a1e7b
jawQ <757050031@qq.com>
2024-04-05 18:46:07 +0800

    feat🎉: 完善ts类型文件

--- index.d.ts
+++ index.d.ts
@@ -4,6 +4,7 @@ declare namespace QuotationExcelHandler {
     template: string,
     errorHandler: (msg: string) => void,
     type?: 'csv' | 'xlsx',
+    clearWhiteSpace = true
   ): void;
   export function excelToJson(file: File, jsonHandler: (data: any[]) => void);
   export const channelTemplateList: { name: string; template: string }[];
